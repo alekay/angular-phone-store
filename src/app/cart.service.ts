5d0a775b855448ce8271acece103add23abe035c
alekay <alekaymusic@gmail.com>
2021-06-22 20:29:13 -0400

    inject httpClient

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Product } from './products';
 
 @Injectable({
@@ -8,6 +9,10 @@ import { Product } from './products';
 export class CartService {
   items: Product[] = [];
 
+  constructor(
+    private http: HttpClient
+  ) {}
+  
   addToCart(product: Product) {
     this.items.push(product);
   }
@@ -20,4 +25,10 @@ export class CartService {
     this.items = [];
     return this.items;
   }
+
+  getShippingPrices() {
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
+  }
 }
+
+
