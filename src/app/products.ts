f809ce266071c1f9635242100bff3a14b92701a6
alekay <alekaymusic@gmail.com>
2021-06-24 18:14:59 -0400

    style: overhaul CSS

--- src/app/products.ts
+++ src/app/products.ts
@@ -2,7 +2,9 @@ export interface Product {
   id: number;
   name: string;
   price: number;
-  description: string;
+  description?: string;
+  // make image optional with ?
+  imgUrl?: string;
 }
 
 export const products = [
@@ -10,18 +12,28 @@ export const products = [
     id: 1,
     name: 'Phone XL',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'A large phone with one of the best screens',
+    imgUrl: "https://i.ibb.co/bR3NTVq/xl-phone.png"
   },
   {
     id: 2,
     name: 'Phone Mini',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras',
+    imgUrl: 'https://i.ibb.co/5vGr6kF/small-phone.png'
   },
   {
     id: 3,
     name: 'Phone Standard',
     price: 299,
-    description: ''
+    imgUrl: 'https://i.ibb.co/wWpxV3W/standard-phone.png'
+
+  },
+  {
+    id: 4,
+    name: 'Watch',
+    price: 59,
+    description: 'A Smart Watch you can sync with your phone',
+    imgUrl: 'https://i.ibb.co/0h4gv7Q/smart-watch.png'
   }
 ];
