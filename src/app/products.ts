77bcedeb1ecb733f9c997a6508cf33464c19ecd0
alekay <alekaymusic@gmail.com>
2021-06-27 15:54:54 -0400

    style: update background image and color pallet for consistency

--- src/app/products.ts
+++ src/app/products.ts
@@ -8,32 +8,34 @@ export interface Product {
 }
 
 export const products = [
+  
   {
     id: 1,
-    name: 'Phone XL',
-    price: 799,
-    description: 'A large phone with one of the best screens',
-    imgUrl: "https://i.ibb.co/bR3NTVq/xl-phone.png"
-  },
-  {
-    id: 2,
-    name: 'Phone Mini',
-    price: 699,
-    description: 'A great phone with one of the best cameras',
-    imgUrl: 'https://i.ibb.co/5vGr6kF/small-phone.png'
-  },
-  {
-    id: 3,
     name: 'Phone Standard',
-    price: 299,
+    price: 399,
+    description: 'Base model',
     imgUrl: 'https://i.ibb.co/wWpxV3W/standard-phone.png'
 
   },
   {
+    id: 2,
+    name: 'Phone Mini',
+    price: 599,
+    description: 'Smaller phone, best camera',
+    imgUrl: 'https://i.ibb.co/5vGr6kF/small-phone.png'
+  },
+  {
+    id: 3,
+    name: 'Phone XL',
+    price: 899,
+    description: 'Larger phone, larger screen',
+    imgUrl: "https://i.ibb.co/bR3NTVq/xl-phone.png"
+  },
+  {
     id: 4,
     name: 'Watch',
-    price: 59,
-    description: 'A Smart Watch you can sync with your phone',
-    imgUrl: 'https://i.ibb.co/0h4gv7Q/smart-watch.png'
+    price: 99,
+    description: 'Smart Watch, can be synced with smart phone',
+    imgUrl: 'https://i.ibb.co/7khB9Q1/smart-watch-2.png'
   }
 ];
---
+++ src/readme.md
@@ -0,0 +1,5 @@
+# This readme is incomplete
+
+## If you want to download or clone the project please make sure to install dependencies listed in package.json
+
+## ng serve will default to localhost port 4200
